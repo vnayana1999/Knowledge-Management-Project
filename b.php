1ccc1c85b09d21f9cf538773015d6bbb6a621f46
vnayana1999 <45292685+vnayana1999@users.noreply.github.com>
2020-04-11 20:50:12 +0530

    Add files via upload

---
+++ b.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <meta name="description" content="">
+    <meta name="author" content="templatemo">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/templatemo-style.css" rel="stylesheet">
+
+
+  </head>
+  <body>
+    <!-- Left column -->
+    <div class="templatemo-flex-row">
+      <div class="templatemo-sidebar">
+        <header class="templatemo-site-header">
+          <div class="square"></div>
+          <br />
+<b>Notice</b>:  Undefined variable: _SESSION in <b>/opt/lampp/htdocs/home/b.php</b> on line <b>35</b><br />
+<h1>Welcome<br></h1><br>        </header>
+        
+        <!-- Search box -->
+        <form class="templatemo-search-form" role="search">
+          <div class="input-group">
+              
+          </div>
+        </form>
+        <div class="mobile-menu-icon">
+            <i class="fa fa-bars"></i>
+          </div>
+        <nav class="templatemo-left-nav">
+          <ul>
+         <ul>
+            <li><a href="login.php" class="active" ><i class="fa fa-home fa-fw"></i>Dashboard</a></li>
+            <li><a href="Placement Drives.php"><i class="fa fa-home fa-fw"></i>Companies</a></li>
+            <!-- <li><a href="#" class="active"><i class="fa fa-users fa-fw"></i>View Students</a></li> -->
+            <li><a href="queries.php"><i class="fa fa-users fa-fw"></i>Applied Companies</a></li>
+            <li><a href="Students Eligibility.php"><i class="fa fa-sliders fa-fw"></i>Students Eligibility Status</a></li>
+            <li><a href="logout.php"><i class="fa fa-eject fa-fw"></i>Sign Out</a></li>
+          </ul>
+        </nav>
+      </div>
+      <!-- Main content -->
+      <div class="templatemo-content col-1 light-gray-bg">
+       
+        <div class="templatemo-content-container">
+
+          <div class="templatemo-content-widget no-padding">
+		  	<a href="bgo.php" class="templatemo-blue-button">View Branchwise</a>
+            <div class="panel panel-default table-responsive">
+
+
+			  <table class="table table-striped table-bordered templatemo-user-table">
+                <thead>
+                  <tr>
+				  <td><a   class="white-text templatemo-sort-by">Approval Date</a></td>
+                    <td><a   class="white-text templatemo-sort-by">First Name</a></td>
+                    <td><a   class="white-text templatemo-sort-by">Last Name </a></td>
+                    <td><a  class="white-text templatemo-sort-by">USN </a></td>
+                    <td><a class="white-text templatemo-sort-by">Mobile  </a></td>
+					   <td><a  class="white-text templatemo-sort-by">Email </a></td>
+                       <td><a class="white-text templatemo-sort-by">DOB  </a></td>
+   <td><a   class="white-text templatemo-sort-by">Sem </a></td>
+   <td><a class="white-text templatemo-sort-by">Branch </a></td>
+   <td><a  class="white-text templatemo-sort-by">SSLC </td>
+   <td><a  class="white-text templatemo-sort-by">PU/Dip </a></td>
+			      <td><a  class="white-text templatemo-sort-by">BE </a></td>
+			      <td><a class="white-text templatemo-sort-by">Backlogs </span></a></td>
+
+
+				  </thead>
+			   </tr>
+
+
+
+
+                </tbody>
+              </table>
+			  </div>
+			  </div>
+			  </div>
+
+
+  <div class="pagination-wrap">
+  <ul class="pagination">
+
+</ul>
+</div>
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
