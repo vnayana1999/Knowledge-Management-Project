056a91b7d800724fc9f5b61687fed786497b3fd3
vnayana1999 <45292685+vnayana1999@users.noreply.github.com>
2020-04-11 21:12:39 +0530

    Add files via upload

---
+++ a.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <meta name="description" content="">
+    <meta name="author" content="templatemo">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/templatemo-style.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <!-- Left column -->
+    <div class="templatemo-flex-row">
+      <div class="templatemo-sidebar">
+        <header class="templatemo-site-header">
+          <div class="square"></div>
+          <?php
+		  $Welcome = "Welcome";
+          echo "<h1>" . $Welcome . "<br>". $_SESSION['pusername']. "</h1>";
+		 echo "<br>";
+
+		  ?>
+        </header>
+        
+        <!-- Search box -->
+        <form class="templatemo-search-form" role="search">
+          <div class="input-group">
+              
+          </div>
+        </form>
+        <div class="mobile-menu-icon">
+            <i class="fa fa-bars"></i>
+          </div>
+        <nav class="templatemo-left-nav">
+          <ul>
+         <ul>
+            <li><a href="login.php" class="active" ><i class="fa fa-fw"></i>Dashboard</a></li>
+            <li><a href="Placement Drives.php"><i class="fa  fa-fw"></i>Companies</a></li>
+            <!-- <li><a href="#" class="active"><i class="fa fa-users fa-fw"></i>View Students</a></li> -->
+            <li><a href="queries.php"><i class="fa  fa-fw"></i>Applied Companies</a></li>
+            <li><a href="Students Eligibility.php"><i class="fa  fa-fw"></i>Students Eligibility Status</a></li>
+            <li><a href="logout.php"><i class="fa  fa-fw"></i>Sign Out</a></li>
+          </ul>
+        </nav>
+      </div>
+      <!-- Main content -->
+      <div class="templatemo-content col-1 light-gray-bg">
+
+        <div class="templatemo-content-container">
+
+          <div class="templatemo-content-widget no-padding">
+		  	<a href="bgo.php" class="templatemo-blue-button">View Branchwise</a>
+            <div class="panel panel-default table-responsive">
+
+
+			  <table class="table table-striped table-bordered templatemo-user-table">
+                <thead>
+                  <tr>
+				  <td><a   class="white-text templatemo-sort-by">Approval Date</a></td>
+                    <td><a   class="white-text templatemo-sort-by">First Name</a></td>
+                    <td><a   class="white-text templatemo-sort-by">Last Name </a></td>
+                    <td><a  class="white-text templatemo-sort-by">USN </a></td>
+                    <td><a class="white-text templatemo-sort-by">Mobile  </a></td>
+					   <td><a  class="white-text templatemo-sort-by">Email </a></td>
+                       <td><a class="white-text templatemo-sort-by">DOB  </a></td>
+   <td><a   class="white-text templatemo-sort-by">Sem </a></td>
+   <td><a class="white-text templatemo-sort-by">Branch </a></td>
+   <td><a  class="white-text templatemo-sort-by">SSLC </td>
+   <td><a  class="white-text templatemo-sort-by">PU/Dip </a></td>
+			      <td><a  class="white-text templatemo-sort-by">BE </a></td>
+			      <td><a class="white-text templatemo-sort-by">Backlogs </span></a></td>
+				     <td><a   class="white-text templatemo-sort-by">History Of Backlogs </span></a></td>
+				     <td><a  class="white-text templatemo-sort-by">Detain Years </a></td>
+
+				  </thead>
+			   </tr>
+
+
+
+
+                </tbody>
+              </table>
+			  </div>
+			  </div>
+			  </div>
+
+
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ footer.php
@@ -0,0 +1,38 @@
+<div class="footer">
+  <div class="container">
+    <div class="col-md-3 ftr_navi ftr">
+      <h3>NAVIGATION</h3>
+      <ul>
+        <li>
+          <a href="../index.php">Home</a>
+        </li>
+        <li>
+          <a href="../student/login.php">Student Login</a>
+        </li>
+        <li>
+          <a href="../companies/login.php">Company Login</a>
+        </li>
+
+        <li>
+          <a href="../admin/login.php">Admin Login</a>
+        </li>
+      </ul>
+    </div>
+    
+    <div class="col-md-3 get_in_touch ftr">
+      <h3>GET IN TOUCH</h3>
+      <p>100 Feet Ring Road,</p>
+      <p>BSK III Stage,</p>
+	  <p>Bangalore-560085</p><br/>
+      <p>+91 80 26721983,</p>
+	  <p>+91 80 26722108
+</p>
+      <a href="mailto:admissions@pes.edu">admissions@pes.edu</a>
+    </div>
+    <div class="col-md-3 get_in_touch ftr">
+
+    </div>
+    
+    </div>
+
+  </div>
